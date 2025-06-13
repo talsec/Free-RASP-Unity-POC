d4a4db669e254e095aa7f23782d26cdc9c37b798
Sirack <sirackh777@gmail.com>
2025-06-13 11:21:32 +0300

    sample example for iOS & Android added

---
+++ Samples/freeRASPTestApp/Scripts/AndroidGame.cs
@@ -0,0 +1,86 @@
+using System;
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+using System.Collections;
+using Google.Play.Common;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Talsec.freeRASPTestApp
+{
+    public class AndroidGame : MonoBehaviour, AndroidThreatDetectedCallback
+    {
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+            // common configs
+            bool isProd = true;
+            string watcherMailAddress = "WATCHER EMAIL ADDRESS";
+
+            // Android related configs
+            string expectedPackageName = "com.unity.freeRASP";
+            string[] expectedSigningCertificateHashBase64 = new string[] { "Tmac/QIomCqEGS1jYqy9cMMrqaitVoZLpjXzCMnt55Q=" };
+            string[] blacklistedPackageNames = new string[] { "com.spotify.music", "com.leavjenn.hews2" };
+            string[] supportedAlternativeStores = new string[] { "com.sec.android.app.samsungapps" };
+
+            // initialize talsec
+            TalsecPlugin.Instance.initAndroidTalsec(expectedPackageName, expectedSigningCertificateHashBase64, 
+                blacklistedPackageNames, supportedAlternativeStores, watcherMailAddress, isProd);
+            TalsecPlugin.Instance.setAndroidCallback(this); // set Android callback
+        }
+
+        // Implementation of IAndroidCallback interface
+        public void onRootDetected()
+        {
+            Debug.Log("Unity - Root detected");
+        }
+
+        public void onTamperDetected()
+        {
+            Debug.Log("Unity - Tamper detected");
+        }
+
+        public void onDebuggerDetected()
+        {
+            Debug.Log("Unity - Debugger detected");
+        }
+
+        public void onEmulatorDetected()
+        {
+            Debug.Log("Unity - Emulator detected");
+        }
+
+        public void onObfuscationIssuesDetected()
+        {
+            Debug.Log("Unity - Obfuscation issues detected");
+        }
+        public void onScreenshotDetected()
+        {
+            Debug.Log("Unity - Screenshot detected");
+        }
+
+        public void onScreenRecordingDetected()
+        {
+            Debug.Log("Unity - Screen recording detected");
+        }
+
+        public void onUntrustedInstallationSourceDetected() {
+            Debug.Log("Unity - Untrusted installation source detected");
+        }
+
+        public void onHookDetected() {
+            Debug.Log("Unity - Hook detected");
+        }
+
+        public void onDeviceBindingDetected() {
+            Debug.Log("Unity - Device binding detected");
+        }
+
+        public void onMalwareDetected(List<SuspiciousAppInfo> malwareList)
+        {
+            Debug.Log("Unity - Malware detected " + malwareList);
+        }
+    }
+}
---
+++ Samples/freeRASPTestApp/Scripts/IOSGame.cs
@@ -0,0 +1,87 @@
+using System;
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+namespace Talsec.freeRASPTestApp
+{
+    public class IOSGame : MonoBehaviour, IOSThreatDetectedCallback
+    {
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+            // common configs
+            bool isProd = true;
+            string watcherMailAddress = "WATCHER EMAIL ADDRESS";
+
+            // iOS related configs
+            string[] appBundleIds = new string[] { "com.unity.freeRASP" };
+            string teamId = "TEAM ID";
+
+            // initialize talsec
+            TalsecPlugin.Instance.initiOSTalsec(appBundleIds, teamId, watcherMailAddress, isProd);
+            TalsecPlugin.Instance.setiOSCallback(this); // set callback
+        }
+
+        // Implementation of IOSThreatDetectedCallback interface
+        public void signatureDetected() {
+            Debug.Log("Unity - Signature detected");
+        }
+
+        public void jailbreakDetected() {
+            Debug.Log("Unity - Jailbreak detected");
+        }
+
+        public void debuggerDetected() {
+            Debug.Log("Unity - Debugger detected");
+        }
+
+        public void runtimeManipulationDetected() {
+            Debug.Log("Unity - Runtime manipulation detected");
+        }
+
+        public void passcodeDetected() {
+            Debug.Log("Unity - Passcode detected");
+        }
+
+        public void passcodeChangeDetected() {
+            Debug.Log("Unity - Passcode change detected");
+        }
+
+        public void simulatorDetected() {
+            Debug.Log("Unity - Simulator detected");
+        }
+
+        public void missingSecureEnclaveDetected() {
+            Debug.Log("Unity - Missing secure enclave detected");
+        }
+
+        public void deviceBindingDetected() {
+            Debug.Log("Unity - Device binding detected");
+        }
+
+        public void unofficialStoreDetected() {
+            Debug.Log("Unity - Unofficial store detected");
+        }
+
+        public void systemVPNDetected() {
+            Debug.Log("Unity - System VPN detected");
+        }
+
+        public void screenshotDetected() {
+            Debug.Log("Unity - Screenshot detected");
+        }
+
+        public void screenRecordingDetected() {
+            Debug.Log("Unity - Screen recording detected");
+        }
+
+        public void deviceIDDetected() {
+            Debug.Log("Unity - Device ID detected");
+        }   
+    }
+}
+
+
+
